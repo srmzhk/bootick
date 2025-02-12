45b01000a7e67a567ef83c96397c651dab6e4932
Timothy Seremyazhko <srmzhk17@gmail.com>
2025-02-13 00:01:32 +0300

    Create the project with main structure and dependencies

---
+++ bootick/src/main/java/com/srmzhk/bootick/BootickApplication.java
@@ -0,0 +1,13 @@
+package com.srmzhk.bootick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootickApplication.class, args);
+    }
+
+}
